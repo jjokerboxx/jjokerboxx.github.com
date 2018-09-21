bd1f785e9ee83e22752abcfdfd9313b25d7ea4de
Episode <dlrlgns58@gmail.com>
2018-09-21 19:46:26 +0900

    js writing io

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var name = document.getElementsByName("title");
+var author = document.getElementsByName("author");
+var description = document.getElementsByName("description");
+
+document.write("<h2>" + name +"</h2>");
+document.write(author);
+document.write(description);
+
