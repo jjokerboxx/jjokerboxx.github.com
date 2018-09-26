0e5ce2cda9517819eede8ad87f8d889af77b6228
Episode <dlrlgns58@gmail.com>
2018-09-26 10:47:55 +0900

    Add files via upload
    edit the code

--- script.js
+++ script.js
@@ -3,7 +3,4 @@ var name = document.getElementsByName("title");
 var author = document.getElementsByName("author");
 var description = document.getElementsByName("description");
 
-document.write("<h2>" + name +"</h2>");
-document.write(author);
-document.write(description);
 
