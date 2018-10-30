84477c57fc7a8aee18e4e6d03df772261cfc0fcc
Episode <dlrlgns58@gmail.com>
2018-10-30 15:35:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var name_ = document.getElementsByName("title");
 
 var description_ = document.getElementsByName("description");
 
+/*    생코 자바스크립트와 html의 분리 설명 + 이벤트 리스너 활용
+var wbtn = document.getElementById('color_btn');
+wbtn.addEventListener('click',function(){
+    document.getElementById('bodycolot').className = "white";
+})
+*/  
+
 function load_pages(param)
 {
     window.open("https://jjokerboxx.github.io/"+param);
@@ -19,3 +26,13 @@ function click_fx(){
     }
     
 };
+
+function clock(){
+
+    string posttime = new Date().toLocaleString();
+    document.getElementsByClassName('post date') = posttime;
+}
+
+function metadesc(){
+    //파싱해야하나?
+}
